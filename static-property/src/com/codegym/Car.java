e5837ea5ed2dfd8b433f79fcea16adb8a95398d9
hieuhvh1994 <hieudaohn94@gmail.com>
2021-09-22 22:04:45 +0700

    add folder static-property

---
+++ static-property/src/com/codegym/Car.java
@@ -0,0 +1,35 @@
+package com.codegym;
+
+public class Car {
+    private String name;
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
