f62b6cac15230eda8aab777f6203c7dad87a1341
hieuhvh1994 <hieudaohn94@gmail.com>
2021-09-23 19:36:24 +0700

    add folder LopCircleVaLopCylinder

---
+++ LopCircleVaLopCylinder/src/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(2.0, "blue");
+        System.out.println(circle);
+    }
+}
