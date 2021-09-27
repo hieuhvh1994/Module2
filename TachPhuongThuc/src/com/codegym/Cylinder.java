07bed8bff3bdf10eb00cb1f6e84d5645e0a47385
hieuhvh1994 <hieudaohn94@gmail.com>
2021-09-27 15:47:39 +0700

    add folder TachPhuongThuc

--- TachPhuongThuc/src/com/codegym/Cylinder.java
+++ TachPhuongThuc/src/com/codegym/Cylinder.java
@@ -3,17 +3,18 @@ package com.codegym;
 public class Cylinder {
     public static double getVolume(int radius, int height){
         double baseArea = getBaseArea(radius);
-        double perimeter = getPerimeter(radius);
+        double perimeter = getPerimeter(radius, 2, Math.PI);
         double volume = perimeter * height + 2 * baseArea;
         return volume;
     }
 
-
-    private static double getPerimeter(int radius) {
-        return 2 * Math.PI  * radius;
+    private static double getPerimeter(int radius, double i, double pi) {
+        return i * pi * radius;
     }
 
     private static double getBaseArea(int radius) {
-        return Math.PI * radius * radius;
+        double baseArea = getPerimeter(radius, Math.PI, radius);
+        return baseArea;
     }
+
 }
