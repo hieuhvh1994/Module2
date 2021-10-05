c1611d7ff997d39b5a82f9f1afbd58b0f30dd96b
hieuhvh1994 <hieudaohn94@gmail.com>
2021-10-05 10:41:13 +0700

    add folder CaiDatThuatToanSapXepChon

---
+++ CaiDatThuatToanSapXepChon/src/SelectionSort.java
@@ -0,0 +1,33 @@
+public class SelectionSort {
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void selectionSort(double[] arr) {
+        for (int i =0; i<arr.length -1; i++) {
+            double currentMin = arr[i];
+            int currentMinIndex = i;
+
+            for (int j=i+1; j<arr.length; j++) {
+                if (currentMin>arr[j]) {
+                    currentMin = arr[j];
+                    currentMinIndex = j;
+
+                }
+            }
+
+            if (currentMinIndex !=i) {
+                arr[currentMinIndex] = arr[i];
+                arr[i] = currentMin;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i=0; i< list.length; i++) {
+            System.out.println(list[i]);
+        }
+
+    }
+}
