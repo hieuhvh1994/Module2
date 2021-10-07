98db8e3b2a3ca45a4569c57a8f374fb8cd57b1ef
hieuhvh1994 <hieudaohn94@gmail.com>
2021-10-07 16:38:20 +0700

    add folder ReadFileExample

---
+++ ReadFileExample/src/ReadFileExample.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            File f = new File(filePath);
+
+            if (!f.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line ="";
+            int sum =0;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+
+            br.close();
+
+            System.out.println("Tổng = " + sum);
+        } catch (Exception e) {
+            System.out.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readFileExample = new ReadFileExample();
+        readFileExample.readFileText(path);
+    }
+}
