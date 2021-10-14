0478590066609b2ddf190f6edf9b741f36168a67
hieuhvh1994 <hieudaohn94@gmail.com>
2021-10-14 20:25:26 +0700

    add folder SuDungBoNhoHieuQuaVoiMemoryPool

---
+++ SuDungBoNhoHieuQuaVoiMemoryPool/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Gun gun = new Gun();
+        System.out.println("Start");
+//        gun.fire();
+        gun.fireInPool();
+        System.out.println("Game Over");
+        System.out.println("Total bullet created: " + Bullet.count);
+    }
+}
